1b5868e881447ef74dd90678c7c8b21ea7b61c33
IT21357930 <99158201+IT21357930@users.noreply.github.com>
2023-08-12 04:01:23 +0530

    all

--- p.js
+++ p.js
@@ -40,15 +40,17 @@ let changeText=()=>{
 changeText();
 setInterval(changeText,3000)
 
-window.onscroll=()=>{
+
 
   
   let header=document.querySelector('header');
   header.classList.toggle('sticky',window.scrollY>100);
-}
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 
 let  menuIcon=document.querySelector('#menu-icon');
 let  navbar=document.querySelector('.navbar');
 menuIcon.onclick=()=>{
   menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }
